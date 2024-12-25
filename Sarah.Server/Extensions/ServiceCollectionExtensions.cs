51d9563ff30a4c46f3f50e12a7da53bbb17f42d9
Christian Fonden <c.fonden@die-rooter.de>
2024-12-26 00:41:33 +0100

    dotnet debugging + nodefactory

--- Sarah.Server/Extensions/ServiceCollectionExtensions.cs
+++ Sarah.Server/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Sarah.API.Interfaces;
 using Sarah.API.Interfaces.Service;
 using Sarah.API.Interfaces.Services;
 using Sarah.Data;
@@ -12,6 +13,7 @@ public static class ServiceCollectionExtensions
     {
         services.AddSingleton(sp => Logger.Instance);
         services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Filename=" + ApplicationDbContext.DatabaseFileName));
+        services.AddSingleton<INodeFactory, Sarah.NodeFactory.NodeFactory>();
         services.AddSingleton<IDBService>(sp => ApplicationDbContext.CreateDefault());
         services.AddSingleton<IDeviceService, Sarah.DeviceService.DeviceService>();
     }
