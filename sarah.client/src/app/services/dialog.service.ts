02794382a1234e2f697caf43c818758c758b1f3e
Christian Fonden <c.fonden@die-rooter.de>
2025-01-14 15:50:31 +0100

    feat: update DialogService to use 'any' type for currentDialog and improve dialog closing logic

--- sarah.client/src/app/services/dialog.service.ts
+++ sarah.client/src/app/services/dialog.service.ts
@@ -8,7 +8,7 @@ export class DialogService {
   constructor() { }
 
   private isDialogOpen: boolean = false;
-  private currentDialog : HTMLElement | null = null;
+  private currentDialog : any | null = null;
 
   closed = new EventEmitter<boolean>();
 
@@ -20,16 +20,15 @@ export class DialogService {
         const bootstrapModal = new (window as any).bootstrap.Modal(modalElement);
         this.currentDialog = bootstrapModal
         bootstrapModal.show();
+        bootstrapModal._element.addEventListener('hidden.bs.modal', () => {
+          this.closeDialog(false);
+        } );
       }
     }
   }
 
   public closeDialog(success: boolean): void {
-    const modalElement = this.currentDialog;
-    if (modalElement && this.isDialogOpen) {
-      const bootstrapModal = new (window as any).bootstrap.Modal(modalElement);
-      bootstrapModal.hide();
-    }
+    this.currentDialog.hide();
     this.isDialogOpen = false;
     this.currentDialog = null;
     this.closed.emit(success);
