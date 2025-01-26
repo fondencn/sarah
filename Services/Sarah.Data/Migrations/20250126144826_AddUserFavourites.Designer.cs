863475f6129e6797af8866c230cfb35430dfc908
Christian Fonden <c.fonden@die-rooter.de>
2025-01-26 15:51:35 +0100

    feat: add migration for UserFavourites table to support user-specific favorite items

--- Services/Sarah.Data/Migrations/20250126144826_AddUserFavourites.Designer.cs
+++ Services/Sarah.Data/Migrations/20250126144826_AddUserFavourites.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sarah.Data;
 
@@ -10,9 +11,11 @@ using Sarah.Data;
 namespace Sarah.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250126144826_AddUserFavourites")]
+    partial class AddUserFavourites
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
