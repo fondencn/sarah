89a54f235b549992c50efb33a6b1ff6283acf372
Christian Fonden <c.fonden@die-rooter.de>
2025-03-13 14:12:07 +0100

    feat: enhance person and room detail views with improved layout and data loading

--- sarah.client/src/app/home/details/person/persondetails/persondetails.component.ts
+++ sarah.client/src/app/home/details/person/persondetails/persondetails.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { PersonDto, PersonsService } from '../../../../services/api-client';
 
 @Component({
   selector: 'app-persondetails',
@@ -7,4 +8,23 @@ import { Component, Input } from '@angular/core';
 })
 export class PersondetailsComponent {
   @Input() id: number = 0;
+
+  personDetails: PersonDto | null = null;
+    lastUpdated : string = "";
+  
+    constructor(private personsService: PersonsService) {}
+  
+    ngOnInit(): void {
+      this.loadPersonDetails();
+    }
+  
+  
+    loadPersonDetails(): void {
+      if (this.id) {
+        this.personsService.apiPersonsIdGet(this.id).subscribe(person => {
+          this.personDetails = person; // Assign the room details to the property
+          this.lastUpdated = new Date().toLocaleString('de-DE');
+        });
+      }
+    }
 }
